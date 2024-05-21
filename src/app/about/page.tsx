3b7234703491d23393131c573aaa26e400e21bbf
Shubhanshu singh <shubhanshusingh1796@gmail.com>
2024-05-21 12:33:40 +0530

    minor updates

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,13 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { motion, useScroll } from "framer-motion";
+import Image from "next/image";
+import { useRef } from "react";
 
 const About = () => {
+  const containerRef: any = useRef();
+  const { scrollXProgress } = useScroll({ container: containerRef });
+
   return (
     <>
       <motion.div
@@ -11,7 +16,7 @@ const About = () => {
         animate={{ y: "0%" }}
         transition={{ duration: 1 }}>
         {/* CONTAINER */}
-        <div className="h-full overflow-y-scroll lg:flex">
+        <div className="h-full overflow-y-scroll lg:flex" ref={containerRef}>
           {/* TEXT CONTAINER */}
           <div className="p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64 lg:w-2/3 lg:pr-0 xl:h-1/2">
             {/* BIOGRAPHY CONTAINER */}
@@ -324,7 +329,9 @@ const About = () => {
             </div>
           </div>
           {/* SVG CONTAINER */}
-          <div className="hidden lg:block w-1/3 xl:h-1/2"></div>
+          <div className="hidden lg:block w-1/3 xl:h-1/2 sticky top-0 z-30">
+            <Image src={"/brain.svg"} alt="Brain" width={100} height={100} />
+          </div>
         </div>
       </motion.div>
     </>
